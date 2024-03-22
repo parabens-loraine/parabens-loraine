9cc4c0d9895b34328d19bf6c5f6527857e162c69
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-22 12:21:35 -0300

    force update

--- src/components/imagens/video.tsx
+++ src/components/imagens/video.tsx
@@ -1,7 +1,8 @@
 export function Video() {
   return (
-    <video width={300} height={300} controls preload={'auto'} loop>
+    <video width={300} height={300} controls loop>
       <source src="/parabens.mp4" type="video/mp4" />
+      Seu navegador não suporta vídeos.
     </video>
   )
 }
